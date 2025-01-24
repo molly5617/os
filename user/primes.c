54d26538abc16ea12a953abedf2984ca521d1bde
molly5617 <mollytsai5617@gmail.com>
2025-01-24 05:10:20 +0000

    123

--- user/primes.c
+++ user/primes.c
@@ -23,5 +23,42 @@ void sieve(int pleft[2])
     else
     {
         close(pright[0]);
+        int buf;
+        while (read(pleft[0], &buf, sizeof(buf)) && buf != -1)
+        {
+            if (buf % p != 0)
+            {
+                write(pright[1], &buf, sizeof(buf));
+            }
+        }
+        buf = -1;
+        write(pright[1], &buf, sizeof(buf));
+        wait(0);
+        exit(0);
     }
+}
+
+int main(int argc, char **argv)
+{
+    int input_pipe[2];
+    pipe(input_pipe);
+    if (fork() == 0)
+    {
+        close(input_pipe[1]);
+        sieve(input_pipe);
+        exit(0);
+    }
+    else
+    {
+        close(input_pipe[0]);
+        int i;
+        for (i = 2; i <= 35; i++)
+        {
+            write(input_pipe[1], &i, sizeof(i));
+        }
+        i = -1;
+        write(input_pipe[1], &i, sizeof(i));
+    }
+    wait(0);
+    exit(0);
 }
