ae6224a30d618f588c99f350ee16b678117bca29
molly5617 <mollytsai5617@gmail.com>
2025-01-28 05:25:01 +0000

    123

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -126,7 +126,30 @@ static uint64 (*syscalls[])(void) = {
     [SYS_close] sys_close,
     [SYS_trace] sys_trace,
 };
-
+const char *kama_syscall_names[] = {
+    [SYS_fork] "fork",
+    [SYS_exit] "exit",
+    [SYS_wait] "wait",
+    [SYS_pipe] "pipe",
+    [SYS_read] "read",
+    [SYS_kill] "kill",
+    [SYS_exec] "exec",
+    [SYS_fstat] "fstat",
+    [SYS_chdir] "chdir",
+    [SYS_dup] "dup",
+    [SYS_getpid] "getpid",
+    [SYS_sbrk] "sbrk",
+    [SYS_sleep] "sleep",
+    [SYS_uptime] "uptime",
+    [SYS_open] "open",
+    [SYS_write] "write",
+    [SYS_mknod] "mknod",
+    [SYS_unlink] "unlink",
+    [SYS_link] "link",
+    [SYS_mkdir] "mkdir",
+    [SYS_close] "close",
+    [SYS_trace] "trace",
+};
 void syscall(void)
 {
   int num;
@@ -136,6 +159,10 @@ void syscall(void)
   if (num > 0 && num < NELEM(syscalls) && syscalls[num])
   {
     p->trapframe->a0 = syscalls[num]();
+    if ((p->kama_syscall_trace >> num) & 1)
+    {
+      printf("%d: syscall %s -> %d\n", p->pid, kama_syscall_names[num], p->trapframe->a0);
+    }
   }
   else
   {
