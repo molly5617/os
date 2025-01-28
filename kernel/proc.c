720e73a6685018f9b5027b0b0a0a783547658a22
molly5617 <mollytsai5617@gmail.com>
2025-01-28 06:39:47 +0000

    finish

--- kernel/proc.c
+++ kernel/proc.c
@@ -314,6 +314,19 @@ int fork(void)
   return pid;
 }
 
+void kama_procnum(uint64 *dst)
+{
+  *dst = 0;
+  struct proc *p;
+  for (p = proc; p < &proc[NPROC]; p++)
+  {
+    if (p->state != UNUSED)
+    {
+      (*dst)++;
+    }
+  }
+}
+
 // Pass p's abandoned children to init.
 // Caller must hold p->lock.
 void reparent(struct proc *p)
